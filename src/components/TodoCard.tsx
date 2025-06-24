097ba91ea00de2dcd04c6f0fefbd1cb12eb76d02
Aaronasgh <aaron.asgharzadeh@gmail.com>
2025-06-25 00:28:16 +0200

    Display JSON response in Card component

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -3,17 +3,25 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import { List, ListItem, Typography } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function TodoCard() {
   const cardMinHeight = 900;
   const cardWidth = 600;
   const cardBorderRadius = "16px";
 
+  type Todo = {
+    id: number;
+    text: string;
+  };
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   const getTodos = async () => {
     const res = await fetch(`http://localhost:4000/todos`);
-    const data = await res.json();
+    const data = (await res.json()) as Todo[];
     console.log(data);
+    setTodos(data);
   };
 
   useEffect(() => {
@@ -44,11 +52,18 @@ export default function TodoCard() {
         </Typography>
         <Typography variant="h5" sx={{ wordBreak: "break-word" }}>
           <List sx={{ listStyleType: "disc", margin: 5 }}>
-            <ListItem sx={{ display: "list-item" }}>Buy Milk</ListItem>
+            {todos.map((todo) => {
+              return (
+                <ListItem key={todo.id} sx={{ display: "list-item" }}>
+                  {todo.text}
+                </ListItem>
+              );
+            })}
+            {/* <ListItem sx={{ display: "list-item" }}>Buy Milk</ListItem>
             <ListItem sx={{ display: "list-item" }}>Do Laundry</ListItem>
             <ListItem sx={{ display: "list-item" }}>Buy Present</ListItem>
             <ListItem sx={{ display: "list-item" }}>Buy Present</ListItem>
-            <ListItem sx={{ display: "list-item" }}>Buy Present</ListItem>
+            <ListItem sx={{ display: "list-item" }}>Buy Present</ListItem> */}
           </List>
         </Typography>
       </CardContent>
