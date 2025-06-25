6bb1edd086cd0413f79e1f59fa5fe2a47495bed4
Aaronasgh <aaron.asgharzadeh@gmail.com>
2025-06-26 01:30:12 +0200

    Code cleanup, removed commented sections

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,6 +1,4 @@
 import Card from "@mui/material/Card";
-// import CardActions from "@mui/material/CardActions";
-// import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Add from "@mui/icons-material/Add";
